fa4537ced6a34b4146d978770f801d75725502b1
harry <harrykirkwood123@gmail.com>
2019-01-21 19:44:57 +0000

    The fixed floating button menu has been added. This will allow the user to sign in, sign out, create a user and manage other users.

--- init.js
+++ init.js
@@ -1,2 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+});
+
 elem = document.querySelector('.dropdown-trigger');
 instance = M.Dropdown.init(elem);
+
+elems = document.querySelectorAll('.tooltipped');
+instances = M.Tooltip.init(elems);
